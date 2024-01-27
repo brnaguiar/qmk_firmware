0cd84d9529149d2d6a58c76cbb1a27935ebb389e
Bruno Aguiar <ext.bruno.aguiar@farfetch.com>
2024-01-27 16:53:04 +0000

    Add test keymaps for the dz60

--- keyboards/dz60/config.h
+++ keyboards/dz60/config.h
@@ -7,3 +7,11 @@
 
 /* VIA related config */
 #define VIA_EEPROM_LAYOUT_OPTIONS_SIZE 2
+
+// brn aguiar changes...
+#define TAPPING_TERM 200 //- term delay... in MiliSeconds
+//#define PERMISSIVE_HOLD // for fast typing...
+#define QUICK_TAP_TERM 170 // to repeat key..
+//#//--#//--#undef  PERMISSIVE_HOLD
+#define HOLD_ON_OTHER_KEY_PRESS
+// #define IGNORE_MOD_TAP_INTERRUPT // -- uncomment if you see lag... and try
--- keyboards/dz60/rules.mk
+++ keyboards/dz60/rules.mk
@@ -10,3 +10,4 @@ NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
+DYNAMIC_TAPPING_TERM_ENABLE = yes
